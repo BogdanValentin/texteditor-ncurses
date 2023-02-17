91251bad8b48f7d4502a247ddc48a84dbe739a98
Bogdan Valentin <bogdanvalentin977@gmail.com>
2023-02-17 16:27:57 +0200

    Adaugare vector cu numarul de caractere per linie

--- sources/calculate.c
+++ sources/calculate.c
@@ -1,15 +1,18 @@
 #include <ncurses.h>
 
-void file_max_lines_and_cols(char filename[], int *lines, int *cols) {
+void file_max_lines_and_cols(char filename[], int *lines, int *cols, int **charsperline) {
     int cols_max = 0;
     *lines = 1;
     *cols = 0;
+    *charsperline = malloc(sizeof(int));
 
     FILE *file = fopen(filename, "rt");
     if(file != NULL) {
         char buffer;
         while(fscanf(file, "%c", &buffer) == 1) {
             if(buffer == '\n') {
+                *charsperline = realloc(*charsperline, *lines * sizeof(int));
+                (*charsperline)[*lines - 1] = *cols;
                 (*lines)++;
                 if(*cols > cols_max) {
                     cols_max = *cols;
@@ -17,13 +20,16 @@ void file_max_lines_and_cols(char filename[], int *lines, int *cols) {
                 *cols = 0;
             } else {
                 (*cols)++;
-            }
+            } 
         }
-        if(*cols > cols_max) {
+        *charsperline = realloc(*charsperline, (*lines) * sizeof(int));
+        (*charsperline)[*lines - 1] = *cols;
+        if(*cols > cols_max) { // tratam cazul cu ultimul rand cel mai lung
             cols_max = *cols;
         }
-        if(cols_max > 0) {
+        if(cols_max > 0) { // tratam cazul cu un singur rand
             *cols = cols_max;
         }
-    } 
+
+    }
 }
